8eebfa348209df38f466a903e0f6a0912f90d79e
rikidox <riccardo.dossena@gmail.com>
2022-03-05 15:01:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it">
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <?php
+                        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+                        header("Cache-Control: post-check=0, pre-check=0", false);
+                        header("Pragma: no-cache");
+                ?>
+                <link rel="stylesheet" type="text/css" href="homestyle-2016-08-20.css" />
+                <meta name="keywords" content="Matematica, LaTeX, TeX, old-arrows, Fisica, Scuola, Scuola Superiore, Insegnamento, Insegnante, Didattica, Riccardo Dossena" />
+                <meta name="Robots" content="index,nofollow" />
+                <meta name="author" content="Riccardo Dossena" />
+                <meta name="description" lang="it" content="Sito personale di Riccardo Dossena, dedicato alla matematica e a LaTeX." />
+                <title>Riccardo Dossena - Pagina web per la scuola superiore</title>
+        </head>
+        <body>
+
+                <div id="wrapper">
+                        <div id="wrap">
+                                <div id="top">
+                                <div class="wraptocenter"><img src="graphics/titolo.png" width="880" alt="Riccardo Dossena"/></div>
+                                </div>
+                                <div class="top_nav">
+                                        <span class="tab1">
+                                                <a href="index.php">Home</a>
+                                        </span>
+                                        <span class="tab0">
+                                                <a href="articoli.php">Articoli</a>
+                                        </span>
+                                        <span class="tab0">
+                                                <a href="materiali.php">Materiali didattici</a>
+                                        </span>
+                                        <span class="tab0">
+                                                <a href="lezioni.php">Lezioni</a>
+                                        </span>
+                                        <span class="tab0">
+                                                <a href="latex.php">L<span class="highA">a</span>T<span class="lowE">e</span>X</a>
+                                        </span>
+                                        <span class="tab0">
+                                                <a href="link.php">Link</a>
+                                        </span>
+                                        <span class="tab2">
+                                                <a href="mailto:riccardo.dossena@unipv.it">Email</a>
+                                        </span>
+                                </div>
+
+                                <div id="main">
+                                        <br/>
+
+                                        <div class="wraptocenter" style="color:#000080; font-size:xx-large"><b>Matematica per
+                                        la scuola superiore</b></div>
+                                        <br/>
+                                        <div class="wraptocenter"><img  width="132" src="graphics/rd.gif" alt="Solidi platonici"/></div>
+
+                                        <p><i>
+                                        “La matematica è la suprema conquista intellettuale dell'uomo, la creazione più originale dello spirito umano. La musica allieta e calma l'animo, la pittura delizia la vista, la poesia suscita le emozioni, la filosofia appaga la mente, l'ingegneria migliora la vita materiale dell'uomo, ma la matematica offre tutti questi valori.”</i>
+                                        </p>
+                                                <p style="text-align: right;">M. Kline, <i>Matematica. La perdita della certezza</i></p>
+
+                                         <br/>
+
+                                        <div class="boxed">
+                                        Benvenuti nel mio sito personale! Sono un insegnante di matematica e fisica della scuola superiore, ma soprattutto un appassionato delle mie materie e di L<span class="highA">a</span>T<span class="lowE">e</span>X. Questo sito è dedicato a queste mie passioni, con particolare attenzione ai risvolti didattici.
+                                        </div>
+
+                                        <br/>
+
+                                        <hr/>
+
+                                        <h2>Ultime news</h2>
+
+                                        <table style="width:100%;font-size:11pt" cellpadding="0">
+                                        <tr>
+                                                <td align="right" valign="top"><strong>28/7/2021</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Articoli/Osservazioni-didattiche-equivalenze-asintotiche.pdf">Le equivalenze asintotiche e la somma di funzioni, osservazioni didattiche</a>” aggiunto nella sezione <a href="articoli.php"><b>Articoli</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>8/3/2021</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Articoli/ANS-Novello-2021.pdf">Introduzione all'analisi non standard</a>” aggiunto nella sezione <a href="articoli.php"><b>Articoli</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>7/1/2021</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Materiali/Insieme-immagine.pdf">Il calcolo dell'insieme immagine</a>” aggiunto nella sezione <a href="materiali.php"><b>Materiali didattici</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>21/08/2020</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Materiali/Hopital-Esempi-Controesempi.pdf">Esempi e controesempi</a>” aggiunto nella sezione <a href="materiali.php"><b>Materiali didattici</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>20/10/2019</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                Costruita la pagina del <a href="corso-latex.php"><b>Corso L<span class="highA">a</span>T<span class="lowE">e</span>X</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>11/3/2019</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “Introduzione all'analisi non standard” aggiunto nella sezione <a href="articoli.php"><b>Articoli</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>28/9/2018</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Articoli/Dossena-modello-numeri-iperreali-Padova2018.pdf">Un modello dei numeri iperreali</a>” aggiunto nella sezione <a href="articoli.php"><b>Articoli</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>14/7/2018</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Articoli/Mondo-iperreale-microscopi-ottici-ANS-Lucca-2016.pdf">Il mondo iperreale attraverso i microscopi ottici</a>” aggiunto nella sezione <a href="articoli.php"><b>Articoli</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>29/6/2017</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                “<a href="Articoli/ANS-INAF-2017.pdf">Introduzione all'analisi non standard</a>” aggiunto nella sezione <a href="articoli.php"><b>Articoli</b></a>.</td>
+                                        </tr>
+
+                                        <tr>
+                                                <td align="right" valign="top"><strong>7/5/2017</strong></td>
+                                                <td valign="top"> &nbsp;-&nbsp;</td>
+                                                <td style="padding-bottom:1em">
+                                                Aggiornata la sezione  <a href="materiali.php"><b>Materiali didattici</b></a> con la presentazione “<a href="Materiali/Lez03-Probabilita.pdf">Calcolo delle probabilità</a>”.</td>
+                                        </tr>
+                                        </table><!--togliere dall'ultimo <td> style="padding-bottom:1em", se necessario-->
+
+                                        <p><a href="news"><strong>Archivio news</strong></a></p>
+
+                                        <hr/>
+                                        <div id="footer">
+                                                <div class="wraptocenter"><img width="15"  src="graphics/mail.gif" alt="Posta"/>&nbsp;
+                                                <a href="mailto:riccardo.dossena@unipv.it">riccardo.dossena@unipv.it</a>
+                                                </div>
+                                                <br/>
+                                                <div class="wraptocenter"><a href="http://www-3.unipv.it/webphilos_lab/dossena"><b>Home
+                                                page universitaria</b></a></div>
+                                                <div class="wraptocenter"><a href="https://independent.academia.edu/RiccardoDossena"><b>La mia pagina Academia.edu</b></a></div>
+                                                <p>
+                                                <a href="http://validator.w3.org/check?uri=referer"><img
+                                                src="graphics/valid-xhtml11.png" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+                                                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                                                <img style="border:0;width:88px;height:31px"
+                                                src="graphics/vcss.gif"
+                                                alt="CSS Valido!" /></a>
+<!-- Histats.com  START  (standard)-->
+<script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+<a href="http://www.histats.com" title="contatore" ><script  type="text/javascript" > try
+{Histats.start(1,3405594,4,0,0,0,"00010000"); Histats.track_hits();} catch(err){}; </script></a>
+<noscript><div><a href="http://www.histats.com" ><img  src="http://sstatic1.histats.com/0.gif?3405594&amp;101"
+alt="contatore" style="border:0px" /></a></div></noscript>
+<!-- Histats.com  END  -->
+                                                </p>
+                                        </div>
+
+
+                                </div>
+
+                        </div>
+
+                </div>
+        </body>
+
+
+</html>
